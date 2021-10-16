117060ae390fa3d5064c049cbbb21e104914751a
Toby <tobydines@outlook.com>
2021-10-16 12:27:13 +1100

    init. Commit

---
+++ cmake-build-debug/CMakeFiles/SENG1120_A3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SENG1120_A3.dir/main.cpp.o"
+  "SENG1120_A3.exe"
+  "SENG1120_A3.pdb"
+  "libSENG1120_A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SENG1120_A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
